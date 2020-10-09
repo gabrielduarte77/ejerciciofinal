144e9deacbc747b86140719928783f204f03bce7
Gabriel Duarte <gabrielduarte77@gmail.com>
2020-10-09 19:48:05 -0300

    Add files via upload

---
+++ jenkins/deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Generamos
+echo app > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+
+# Transifere el archivo
+scp -i /tmp/tf_pipeline/class_key.pem /tmp/.auth ubuntu@ec2-18-220-70-62.us-east-2.compute.amazonaws.com:/tmp/.auth
+scp -i /tmp/tf_pipeline/class_key.pem /tmp/jenkins/deploy/publish ubuntu@ec2-18-220-70-62.us-east-2.compute.amazonaws.com:/tmp/publish
+ssh -i /tmp/tf_pipeline/class_key.pem ubuntu@ec2-18-220-70-62.us-east-2.compute.amazonaws.com /tmp/publish
